68d50d1471aaaef2f0589ab5279bff7994cdd406
trisdauvergne <tristan.dauvergne@gmail.com>
2021-11-07 18:42:37 +0100

    Styling dishSection component

--- App.js
+++ App.js
@@ -7,16 +7,8 @@ import {
   FugazOne_400Regular 
 } from '@expo-google-fonts/fugaz-one';
 import { 
-  // Lato_100Thin,
-  // Lato_100Thin_Italic,
-  // Lato_300Light,
-  // Lato_300Light_Italic,
   Lato_400Regular,
-  // Lato_400Regular_Italic,
   Lato_700Bold,
-  // Lato_700Bold_Italic,
-  // Lato_900Black,
-  // Lato_900Black_Italic 
 } from '@expo-google-fonts/lato'
 import AppLoading from 'expo-app-loading';
 
--- app/components/DishSection.js
+++ app/components/DishSection.js
@@ -2,24 +2,48 @@ import React from 'react'
 import {
     View,
     Text,
+    StyleSheet
 } from 'react-native'
 
 const DishSection = ({ dish }) => {
     return (
-        <View>
-            <Text>
+        <View
+            style={styles.dishContainer}
+        >
+            <Text
+                style={styles.dishName}
+            >
                 {dish.name}
             </Text>
             {dish.description !== '' &&
-                <Text>
+                <Text
+                    style={styles.dishCopy}
+                >
                     {dish.description}
                 </Text>
             }
-            <Text>
+            <Text
+                style={styles.dishCopy}
+            >
                 {dish.price}
             </Text>
         </View>
     )
 };
+
+const styles = StyleSheet.create({
+    dishContainer: {
+        marginBottom: 10,
+    },
+    dishName: {
+        fontFamily: 'lato-bold',
+        fontSize: 17,
+        marginBottom: 4,
+    },
+    dishCopy: {
+        fontFamily: 'lato-regular',
+        fontSize: 14,
+    }
+})
 
 export default DishSection
