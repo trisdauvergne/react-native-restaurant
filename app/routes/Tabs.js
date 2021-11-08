d16196d8dcdefaeef412412e60eb8432851d339e
trisdauvergne <tristan.dauvergne@gmail.com>
2021-11-08 16:03:46 +0100

    Added basic styling to tabBarLabels

--- app/routes/Tabs.js
+++ app/routes/Tabs.js
@@ -11,26 +11,45 @@ import {
 import WelcomeScreen from '../screens/WelcomeScreen';
 import FoodMenuScreen from '../screens/FoodMenuScreen';
 import DrinksMenuScreen from '../screens/DrinksMenuScreen';
+import ReservationScreen from '../screens/ReservationScreen';
 
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
     return (
-        <NavigationContainer
-            
-        >
+        <NavigationContainer>
             <Tab.Navigator
                 screenOptions={{
                     headerShown: false,
+                    tabBarShowLabel: true,
+                    tabBarLabelStyle: {
+                        fontSize: 20,
+                        fontFamily: 'fugaz-one',
+                        color: '#FAF9F6',
+                        marginBottom: 20,
+                        // backgroundColor: '#FAF9F6',
+                        paddingHorizontal: 5,
+                    },
+                    tabBarStyle: {
+                        backgroundColor: 'transparent',
+                        borderTopWidth: 0,
+                    },
+                    tabBarIconStyle: {
+                        display: 'none'
+                    }
                 }}
                 sceneContainerStyle={{
                     backgroundColor: 'transparent'
                 }}
                 initialRouteName='Welcome'
             >
-                <Tab.Screen name='Welcome' component={WelcomeScreen} />
+                <Tab.Screen
+                    name='Home'
+                    component={WelcomeScreen}
+                />
                 <Tab.Screen name='Food' component={FoodMenuScreen} />
                 <Tab.Screen name='Drink' component={DrinksMenuScreen} />
+                <Tab.Screen name='Booking' component={ReservationScreen} />
             </Tab.Navigator>
         </NavigationContainer>
     )
